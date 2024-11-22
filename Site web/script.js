86aabdba8fb8ec8bc037bb725afd6d7619c3d291
Cap0ral-kudo <caporalkudo@gmail.Com>
2024-11-23 00:29:47 +0100

    Add files via upload

---
+++ Site web/script.js
@@ -0,0 +1,8 @@
+// Mise à jour de la jauge de progression
+document.addEventListener('scroll', () => {
+    const scrollTop = document.documentElement.scrollTop;
+    const scrollHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    const progress = (scrollTop / scrollHeight) * 100;
+    document.getElementById('progress-bar').style.width = progress + '%';
+  });
+  
